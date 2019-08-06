8b556271cf13200ca83069672edf55dd98744dc0
yxc023 <yxc023@qq.com>
2019-08-06 22:31:29 +0800

    update gradle 5.5.1

--- blueprint-order/src/main/java/com/yangxiaochen/blueprint/order/service/exception/OrderException.java
+++ blueprint-order/src/main/java/com/yangxiaochen/blueprint/order/service/exception/OrderException.java
@@ -1,4 +1,25 @@
 package com.yangxiaochen.blueprint.order.service.exception;
 
-public class OrderException extends RuntimeException {
+import com.yangxiaochen.exception.core.BaseRichRuntimeException;
+
+public class OrderException extends BaseRichRuntimeException {
+
+    public OrderException() {
+    }
+
+    public OrderException(String message) {
+        super(message);
+    }
+
+    public OrderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderException(Throwable cause) {
+        super(cause);
+    }
+
+    public OrderException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
